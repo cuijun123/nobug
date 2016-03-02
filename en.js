96ebe76d861d3af1975d624dd531abaa35af4f28
WoeOm <1015996366@qq.com>
2016-03-02 17:14:49 +0800

    add

---
+++ en.js
@@ -0,0 +1,12 @@
+
+//
+// Dear Bob：
+//
+// When you see this note, I know you are very painful
+// the following code never changed,
+// I also changed the code, I am leaving now
+//
+//
+
+
+//When I wrote this, only God and I understood what I was doing //Now, God only knows
